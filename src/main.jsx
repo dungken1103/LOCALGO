5984eb6a7ed9073cbc4a1e162e2b4499c2f3fd7e
Dung <dungtdhe186461@fpt.edu.vn>
2026-02-26 22:01:02 +0700

    1

--- src/main.jsx
+++ src/main.jsx
@@ -10,22 +10,22 @@ import { initGA, logPageView, logScrollDepth } from './services/googleAnalytics'
 
 const queryClient = new QueryClient();
 
-// Khởi tạo Google Analytics với Measurement ID
-initGA('G-09DG9LRV5G');
+// // Khởi tạo Google Analytics với Measurement ID
+// initGA('G-09DG9LRV5G');
 
-// Theo dõi pageview khi ứng dụng được tải
-logPageView(window.location.pathname);
+// // Theo dõi pageview khi ứng dụng được tải
+// logPageView(window.location.pathname);
 
-// Theo dõi mức độ cuộn trang
-window.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY;
-  const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrollPercent = Math.round((scrollTop / docHeight) * 100);
+// // Theo dõi mức độ cuộn trang
+// window.addEventListener('scroll', () => {
+//   const scrollTop = window.scrollY;
+//   const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   const scrollPercent = Math.round((scrollTop / docHeight) * 100);
 
-  if (scrollPercent % 25 === 0) {
-    logScrollDepth(scrollPercent);
-  }
-});
+//   if (scrollPercent % 25 === 0) {
+//     logScrollDepth(scrollPercent);
+//   }
+// });
 
 const Main = () => (
   <StrictMode>
